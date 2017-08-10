f1873794a553090e6b75a3a197e08c5dc978c448
K_A_S <askuzyk@gmail.com>
2017-08-10 13:24:37 +0300

    set SyliusShopApiPlugin and SyliusElasticSearchPlugin not obligatory

--- src/DependencyInjection/CatalogPromotionExtension.php
+++ src/DependencyInjection/CatalogPromotionExtension.php
@@ -7,6 +7,7 @@ namespace Urbanara\CatalogPromotionPlugin\DependencyInjection;
 use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
 use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
@@ -37,12 +38,20 @@ final class CatalogPromotionExtension extends AbstractResourceExtension implemen
         $config = $this->processConfiguration($this->getConfiguration([], $container), $container->getExtensionConfig($this->getAlias()));
         $this->registerResources('urbanara_catalog_promotion', SyliusResourceBundle::DRIVER_DOCTRINE_ORM, $config['resources'], $container);
 
-        $this->prependElasticSearchPlugin($container);
-        $this->prependShopApiPlugin($container);
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+
+        $this->prependElasticSearchPlugin($container, $loader);
+        $this->prependShopApiPlugin($container, $loader);
     }
 
-    private function prependElasticSearchPlugin(ContainerBuilder $container): void
+    private function prependElasticSearchPlugin(ContainerBuilder $container, LoaderInterface $loader): void
     {
+        if (!$container->hasExtension('sylius_elastic_search')) {
+            return;
+        }
+
+        $loader->load('services/integrations/elastic_search.xml');
+
         $container->prependExtensionConfig('sylius_elastic_search', [
             'document_classes' => [
                 'product' => ProductDocument::class,
@@ -54,13 +63,22 @@ final class CatalogPromotionExtension extends AbstractResourceExtension implemen
         ]);
     }
 
-    private function prependShopApiPlugin(ContainerBuilder $container): void
+    private function prependShopApiPlugin(ContainerBuilder $container, LoaderInterface $loader): void
     {
-        $container->prependExtensionConfig('shop_api', [
-            'view_classes' => [
-                'price' => ShopApiView\PriceView::class,
-                'product_variant' => ShopApiView\ProductVariantView::class,
-            ],
-        ]);
+        if (!$container->hasExtension('shop_api')) {
+            return;
+        }
+
+        $loader->load('services/integrations/shop_api.xml');
+
+        $container->prependExtensionConfig(
+            'shop_api',
+            [
+                'view_classes' => [
+                    'price' => ShopApiView\PriceView::class,
+                    'product_variant' => ShopApiView\ProductVariantView::class,
+                ],
+            ]
+        );
     }
 }
